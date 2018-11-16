a71510109d559d0526164141f0ec4b3cd90571f5
shuyy <490899514@qq.com>
2018-11-16 16:21:11 +0800

    修改了目录结构

---
+++ modules/community-yingtan/community-yingtan-server/src/main/java/com/mit/community/module/bigdata/schedule/AccessControlSchedule.java
@@ -0,0 +1,17 @@
+package com.mit.community.module.bigdata.schedule;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 门禁 定时器
+ *
+ * @author shuyy
+ * @date 2018/11/16
+ * @company mitesofor
+ */
+@Component
+public class AccessControlSchedule {
+
+
+
+}
