0e4cb185b229c4852c4b5f5992d13928e52beaef
shuyy <490899514@qq.com>
2018-11-16 11:19:15 +0800

    修改了数据源

---
+++ modules/community-yingtan/community-yingtan-server/src/test/java/com/mit/community/importdata/DnakeInvokeTest.java
@@ -0,0 +1,110 @@
+package com.mit.community.importdata;
+
+import com.dnake.common.DnakeWebApiUtil;
+import com.dnake.constant.DnakeConstants;
+import com.dnake.constant.DnakeWebConstants;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+/**
+ * dnake 调用测试
+ *
+ * @author shuyy
+ * @date 2018/11/13
+ * @company mitesofor
+ */
+public class DnakeInvokeTest {
+
+    @Test
+    public void getAccessControlList(){
+        long startTime = System.currentTimeMillis();
+        DnakeConstants.choose(DnakeConstants.MODEL_PRODUCT);
+        String url = "/v1/device/getAccessControlList";
+        HashMap<String, Object> map = new HashMap<>();
+//        map.put("communityCode", "0afe12f3e3b4476689750035f9ee698d");
+        map.put("deviceName", "凯翔演示-进门");
+//        map.put("deviceNum", "AB900DX8880285879170");
+        map.put("pageSize", "10");
+        map.put("pageNum", "1");
+        map.put("accountId", DnakeWebConstants.ACCOUNT_ID);
+        String result = DnakeWebApiUtil.invoke(url, map);
+        System.out.println(result);
+        long end = System.currentTimeMillis();
+        System.out.println(end - startTime);
+    }
+    @Test
+    public void queryVisitorInfo(){
+        long startTime = System.currentTimeMillis();
+        DnakeConstants.choose(DnakeConstants.MODEL_PRODUCT);
+        String url = "/v1/visitor/queryVisitorInfo";
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("communityCode", "ab497a8a46194311ad724e6bf79b56de");
+        map.put("visitorId", "37137");
+        String result = DnakeWebApiUtil.invoke(url, map);
+        System.out.println(result);
+        long end = System.currentTimeMillis();
+        System.out.println(end - startTime);
+    }
+    @Test
+    public void getHouseholdList(){
+        long startTime = System.currentTimeMillis();
+        DnakeConstants.choose(DnakeConstants.MODEL_PRODUCT);
+        String url = "/v1/household/getHouseholdList";
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("communityCode", "ab497a8a46194311ad724e6bf79b56de");
+        map.put("pageNum", "1");
+        map.put("pageSize", "100");
+        String result = DnakeWebApiUtil.invoke(url, map);
+        System.out.println(result);
+        long end = System.currentTimeMillis();
+        System.out.println(end - startTime);
+    }
+    @Test
+    public void queryHouseholdInfoByName(){
+        long startTime = System.currentTimeMillis();
+        DnakeConstants.choose(DnakeConstants.MODEL_PRODUCT);
+        String url = "/v1//household/queryHouseholdInfoByName";
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("communityCode", "ab497a8a46194311ad724e6bf79b56de");
+        map.put("householdName", "舒园园");
+        String result = DnakeWebApiUtil.invoke(url, map);
+        System.out.println(result);
+        long end = System.currentTimeMillis();
+        System.out.println(end - startTime);
+    }
+
+    @Test
+    public void getVisitorList(){
+        long startTime = System.currentTimeMillis();
+        DnakeConstants.choose(DnakeConstants.MODEL_PRODUCT);
+        String url = "/v1/visitor/getVisitorList";
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("communityCode", "ab497a8a46194311ad724e6bf79b56de");
+        map.put("pageSize", "100");
+        map.put("pageNum", "1");
+
+//        map.put("householdName", "舒园园");
+        String result = DnakeWebApiUtil.invoke(url, map);
+        System.out.println(result);
+        long end = System.currentTimeMillis();
+        System.out.println(end - startTime);
+    }
+
+    @Test
+    public void queryHouseholdInfo(){
+        long startTime = System.currentTimeMillis();
+        DnakeConstants.choose(DnakeConstants.MODEL_PRODUCT);
+        String url = "/v1/household/queryHouseholdInfo";
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("communityCode", "ab497a8a46194311ad724e6bf79b56de");
+        map.put("id", "37137");
+        String result = DnakeWebApiUtil.invoke(url, map);
+        System.out.println(result);
+        long end = System.currentTimeMillis();
+        System.out.println(end - startTime);
+    }
+
+   
+
+}
