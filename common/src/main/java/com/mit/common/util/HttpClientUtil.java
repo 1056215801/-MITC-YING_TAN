304682e45df613a05c5139b593e960bf00968486
shuyy <490899514@qq.com>
2018-12-28 16:44:53 +0800

    修改了登录

--- common/src/main/java/com/mit/common/util/HttpClientUtil.java
+++ common/src/main/java/com/mit/common/util/HttpClientUtil.java
@@ -75,6 +75,56 @@ public class HttpClientUtil {
     }
 
     /**
+     * 发送get请求,并返回JSON数据 Https
+     *
+     * @param url 路径
+     * @return JSON数据
+     */
+    public static String getMethodRequestResponse(String url, String cookie) {
+        String result;
+        HttpClient httpClient = initHttpClient();
+        String charSetName = "utf-8";
+        // 创建一个方法实例.
+        GetMethod getMethod = new GetMethod(url);
+        getMethod.addRequestHeader( "Connection", "close");
+        getMethod.setRequestHeader("cookie", cookie);
+        // 提供定制的重试处理程序是必要的
+        getMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
+        BufferedReader reader = null;
+        try {
+            // 执行getMethod.
+            int status = httpClient.executeMethod(getMethod);
+            if (HttpStatus.SC_OK == status) {
+                reader = new BufferedReader(
+                        new InputStreamReader(getMethod.getResponseBodyAsStream(), charSetName));
+                StringBuilder stringBuffer = new StringBuilder();
+                String str;
+                while ((str = reader.readLine()) != null) {
+                    stringBuffer.append(str);
+                }
+                result = stringBuffer.toString();
+                reader.close();
+            } else {
+                result = "error:" + status;
+                log.error("HttpClientUtil get to:[{}] error!", url);
+            }
+        } catch (Exception e) {
+            result = "error:" + e;
+            log.error("HttpClientUtil get to:[" + url + "] error!", e);
+        } finally {
+            if(reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            getMethod.releaseConnection();
+        }
+        return result;
+    }
+
+    /**
      * 初始化通信为Https
      *
      * @return HttpClient
