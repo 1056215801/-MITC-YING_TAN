2c8e4a01719b15c0784f8c7c7fd6881124f58a61
Cheng Deng <chengdeng@example.com>
2019-02-28 11:14:37 +0800

    优化代码

--- modules/app-community-wanli/app-community-wanli-server/src/main/java/com/mit/CommunityBootstrap.java
+++ modules/app-community-wanli/app-community-wanli-server/src/main/java/com/mit/CommunityBootstrap.java
@@ -55,25 +55,6 @@ public class CommunityBootstrap {
         new SpringApplicationBuilder(CommunityBootstrap.class).web(WebApplicationType.SERVLET).run(args);
     }
 
-//    @Bean
-//    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-//        StringRedisTemplate template = new StringRedisTemplate(factory);
-//        //定义key序列化方式
-//        //RedisSerializer<String> redisSerializer = new StringRedisSerializer();//Long类型会出现异常信息;需要我们上面的自定义key生成策略，一般没必要
-//        //定义value的序列化方式
-//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-//        ObjectMapper om = new ObjectMapper();
-//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//        jackson2JsonRedisSerializer.setObjectMapper(om);
-//
-//        // template.setKeySerializer(redisSerializer);
-//        template.setValueSerializer(jackson2JsonRedisSerializer);
-//        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-//        template.afterPropertiesSet();
-//        return template;
-//    }
-
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         StringRedisTemplate template = new StringRedisTemplate(factory);
