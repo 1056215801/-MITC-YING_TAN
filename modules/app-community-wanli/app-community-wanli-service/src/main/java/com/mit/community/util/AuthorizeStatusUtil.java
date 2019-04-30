f1bbcef45a93b1df4070700147217957d83569ea
root <1358311815@qq.com>
2019-04-30 15:06:06 +0800

    解决冲突

--- modules/app-community-wanli/app-community-wanli-service/src/main/java/com/mit/community/util/AuthorizeStatusUtil.java
+++ modules/app-community-wanli/app-community-wanli-service/src/main/java/com/mit/community/util/AuthorizeStatusUtil.java
@@ -5,13 +5,20 @@ import java.util.List;
 
 /**
  * 房屋授权类型判断
+ *
  * @author hsl
  * @date 2019-03-30
  * @company mitesofor
  */
 public class authorizeStatusUtil {
 
-    public static List<Integer> Contrast(Integer status){
+    /**
+     * 判断授权类型
+     *
+     * @param status
+     * @return
+     */
+    public static List<Integer> Contrast(Integer status) {
         /**
          * 未授权：0;卡：1;roomNumapp：10;人脸：100）；例:人脸和卡授权：101
          */
@@ -19,28 +26,28 @@ public class authorizeStatusUtil {
         //转为二进制
         String BinaryString = null;
         Integer BinaryInteger = null;
-        if(status != null){
+        if (status != null) {
             BinaryString = Integer.toBinaryString(status);
             BinaryInteger = Integer.valueOf(BinaryString);
         }
-        if(BinaryInteger != null){
-            if(BinaryInteger == 1||BinaryInteger == 10||BinaryInteger == 100){
+        if (BinaryInteger != null) {
+            if (BinaryInteger == 1 || BinaryInteger == 10 || BinaryInteger == 100) {
                 list.add(BinaryInteger);
-            }else{
+            } else {
                 ///只有枚举的办法了，笨了点，辛苦点
-                if(BinaryInteger == 11){
+                if (BinaryInteger == 11) {
                     list.add(BinaryInteger - 10);
                     list.add(BinaryInteger - 1);
                 }
-                if(BinaryInteger == 101){
+                if (BinaryInteger == 101) {
                     list.add(BinaryInteger - 100);
                     list.add(BinaryInteger - 1);
                 }
-                if(BinaryInteger == 110){
+                if (BinaryInteger == 110) {
                     list.add(BinaryInteger - 100);
                     list.add(BinaryInteger - 10);
                 }
-                if(BinaryInteger == 111){
+                if (BinaryInteger == 111) {
                     list.add(BinaryInteger - 100);
                     list.add(BinaryInteger - 10);
                     list.add(BinaryInteger - 1);
@@ -48,5 +55,29 @@ public class authorizeStatusUtil {
             }
         }
         return list;
+    }
+
+    /**
+     * 计算授权类型
+     *
+     * @param appAuthFlag
+     * @param faceAuthFlag
+     * @param cardAuthFlag
+     * @return
+     */
+    public static Integer GetAuthStatus(Integer appAuthFlag, Integer faceAuthFlag, Integer cardAuthFlag) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        if (appAuthFlag != null && appAuthFlag == 1) {
+            a = 2;
+        }
+        if (faceAuthFlag != null && faceAuthFlag == 1) {
+            b = 4;
+        }
+        if (cardAuthFlag != null && cardAuthFlag == 1) {
+            c = 1;
+        }
+        return (a + b + c);
     }
 }
