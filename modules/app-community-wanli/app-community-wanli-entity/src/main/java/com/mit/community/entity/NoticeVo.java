39470ec15170ba51d8d59b4f5c81efe15e1b3816
root <1358311815@qq.com>
2019-05-17 15:50:05 +0800

    app公告

---
+++ modules/app-community-wanli/app-community-wanli-entity/src/main/java/com/mit/community/entity/NoticeVo.java
@@ -0,0 +1,10 @@
+package com.mit.community.entity;
+
+/**
+ * @Author: HuShanLin
+ * @Date: Create in 2019/5/7 15:36
+ * @Company mitesofor
+ * @Description:
+ */
+public class NoticeVo {
+}
---
+++ modules/app-community-wanli/app-community-wanli-entity/src/main/java/com/mit/community/entity/UploadFileVo.java
@@ -0,0 +1,10 @@
+package com.mit.community.entity;
+
+/**
+ * @Author: HuShanLin
+ * @Date: Create in 2019/5/7 9:19
+ * @Company mitesofor
+ * @Description:
+ */
+public class UploadFileVo {
+}
