089e795864873ab14cd7c741909e17149125ca38
PC201906131219\Administrator <1037250169@qq.com>
2019-08-28 12:43:48 +0800

    可以运行的版本

--- modules/app-community-wanli/app-community-wanli-service/src/main/java/com/mit/community/service/ZoneService.java
+++ modules/app-community-wanli/app-community-wanli-service/src/main/java/com/mit/community/service/ZoneService.java
@@ -2,12 +2,25 @@ package com.mit.community.service;
 
 import com.ace.cache.annotation.Cache;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.mit.community.entity.Building;
 import com.mit.community.entity.Zone;
+import com.mit.community.mapper.BuildingMapper;
 import com.mit.community.mapper.ZoneMapper;
+import com.mit.community.util.Result;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * 分区业务处理层
@@ -17,12 +30,15 @@ import java.util.List;
  * <p>Company: mitesofor </p>
  */
 @Service
-public class ZoneService {
+public class ZoneService extends ServiceImpl<ZoneMapper,Zone> {
     @Autowired
     private ZoneMapper zoneMapper;
+    @Autowired
+    private BuildingMapper buildingMapper;
 
     /**
      * 查询分区信息，通过小区code
+     *
      * @param communityCode 小区code
      * @return 分区列表
      * @author Mr.Deng
@@ -37,6 +53,7 @@ public class ZoneService {
 
     /**
      * 查询分区信息，通过分区
+     *
      * @param zoneId 分区Id
      * @return 分区信息
      * @author Mr.Deng
@@ -54,7 +71,108 @@ public class ZoneService {
         }
         return zones.get(0);
     }
-    public Zone getByCommunityName(String communityName) {
+
+    public void save(String zoneName, Integer zoneType, String zoneCode, Integer sort, String communityCode) {
+        EntityWrapper<Zone> wrapper = new EntityWrapper<>();
+        wrapper.orderBy("zone_id", false);
+        wrapper.last("limit 1");
+        Zone zoneIdMax = baseMapper.selectList(wrapper).get(0);
+        Zone zone = new Zone();
+        zone.setZoneId(zoneIdMax.getZoneId() + 1);
+        zone.setCommunityCode(communityCode);
+        zone.setZoneName(zoneName);
+        zone.setZoneType(zoneType);
+        zone.setZoneCode(zoneCode);
+        zone.setSort(sort);
+        zone.setZoneStatus(1);
+        zone.setGmtCreate(LocalDateTime.now());
+        zone.setGmtModified(LocalDateTime.now());
+        zoneMapper.insert(zone);
+    }
+
+    public void update(String zoneName, Integer zoneType, Integer sort, Integer id) {
+        Zone zone = zoneMapper.selectById(id);
+        if (zone != null) {
+            zone.setZoneName(zoneName);
+            zone.setZoneType(zoneType);
+            zone.setSort(sort);
+            zone.setGmtModified(LocalDateTime.now());
+
+
+        }
+        zoneMapper.update(zone, null);
+    }
+
+    public Page<Zone> getZoneList(String zoneName, Integer zoneType, Integer zoneStatus, Integer pageNum, Integer pageSize, String communityCode) {
+        Page<Zone> page = new Page<>(pageNum, pageSize);
+        EntityWrapper<Zone> wrapper = new EntityWrapper<>();
+        if (!StringUtils.isEmpty(zoneName)) {
+            wrapper.like("zone_name", zoneName);
+        }
+        if (zoneType != null) {
+            wrapper.eq("zone_type", zoneType);
+        }
+        if (zoneStatus != null) {
+            wrapper.eq("zone_status", zoneStatus);
+        }
+        if (StringUtils.isNotEmpty(communityCode)) {
+            wrapper.eq("community_code", communityCode);
+        }
+        wrapper.orderBy("sort", true);
+        List<Zone> zones = zoneMapper.selectPage(page, wrapper);
+        page.setRecords(zones);
+        return page;
+    }
+
+    public Result delete(List<Integer> ids) {
+        for (int i = 0; i < ids.size(); i++) {
+            EntityWrapper<Zone> entityWrapper = new EntityWrapper<>();
+            entityWrapper.eq("id", ids.get(i));
+            Zone zone = zoneMapper.selectList(entityWrapper).get(0);
+            EntityWrapper<Building> wrapper = new EntityWrapper<>();
+            wrapper.eq("zone_id", zone.getZoneId());
+            List<Building> buildingList = buildingMapper.selectList(wrapper);
+            if (buildingList.size() > 0) {
+                return Result.error("分区已经被楼栋关联不能删除");
+            }
+        }
+        for (int i = 0; i < ids.size(); i++) {
+
+            EntityWrapper<Zone> wrapper1 = new EntityWrapper<>();
+            zoneMapper.deleteById(ids.get(i));
+        }
+        return Result.success("删除成功");
+    }
+
+    public Result stop(List<Integer> idList) {
+
+        for (int i = 0; i < idList.size(); i++) {
+            EntityWrapper<Zone> entityWrapper = new EntityWrapper<>();
+            entityWrapper.eq("id", idList.get(i));
+            Zone zone = zoneMapper.selectList(entityWrapper).get(0);
+            EntityWrapper<Building> wrapper = new EntityWrapper<>();
+            wrapper.eq("zone_id", zone.getZoneId());
+            List<Building> buildingList = buildingMapper.selectList(wrapper);
+            if (buildingList.size() > 0) {
+                return Result.error("分区楼栋已经被关联不能禁用");
+            }
+        }
+        for (int i = 0; i < idList.size(); i++) {
+            Zone zone = new Zone();
+            zone.setZoneStatus(0);
+            EntityWrapper<Zone> wrapper = new EntityWrapper<>();
+            wrapper.eq("id", idList.get(i));
+            baseMapper.update(zone, wrapper);
+        }
+        return Result.success("禁用成功");
+    }
+
+    public void enable(List<Integer> idList) {
+        for (int i = 0; i < idList.size(); i++) {
+            zoneMapper.updateStatus(1, idList.get(i));
+        }
+    }
+    public Zone getByCommunityName (String communityName){
         EntityWrapper<Zone> wrapper = new EntityWrapper<>();
         wrapper.eq("zone_name", communityName);
         List<Zone> zones = zoneMapper.selectList(wrapper);
@@ -64,10 +182,10 @@ public class ZoneService {
         return zones.get(0);
     }
 
-    public Zone getByCommunityCode(String communityCode) {
+    public Zone getByCommunityCode (String communityCode){
         EntityWrapper<Zone> wrapper = new EntityWrapper<>();
         wrapper.eq("community_code", communityCode);
-        wrapper.eq(false,"zone_name","默认分区");
+        wrapper.eq(false, "zone_name", "默认分区");
         List<Zone> zones = zoneMapper.selectList(wrapper);
         if (zones.isEmpty()) {
             return null;
@@ -75,7 +193,7 @@ public class ZoneService {
         return zones.get(0);
     }
 
-    public Zone getByZoneId( Integer zoneId) {
+    public Zone getByZoneId (Integer zoneId){
         EntityWrapper<Zone> wrapper = new EntityWrapper<>();
         wrapper.eq("zone_id", zoneId);
         wrapper.eq("zone_status", 1);
@@ -84,5 +202,6 @@ public class ZoneService {
             return null;
         }
         return zones.get(0);
+
     }
 }
